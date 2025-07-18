5455644f1dfe5a36eedd59b40f5ff5d448ab2dba
pFlugo1942 <freddy.lugo@phrase.com>
2025-07-18 18:14:29 -0400

    Update generate_phrase_config.sh

--- scripts/generate_phrase_config.sh
+++ scripts/generate_phrase_config.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Specify the base directory
-base_dir="./instashopper-android/shared"
+base_dir="./src"
 
 # Initialize the configuration file
 echo "phrase:" > ./push_config.yml
-echo "  project_id: 15d32bafd4ffe92f156bcca0549a07e6" >> ./push_config.yml
-echo "  file_format: xml" >> ./push_config.yml
+echo "  project_id: aa17ae5c0c19b1bd0d3cbe9e779d210a" >> ./push_config.yml
+echo "  file_format: simple_json" >> ./push_config.yml
 echo "  push:" >> ./push_config.yml
 
-# Loop over all XML files in nested directories
-find "$base_dir" -type f -name "*.xml" | while read -r file_path; do
+# Loop over all JSON files in nested directories
+find "$base_dir" -type f -name "*.json" | while read -r file_path; do
     # Get the full folder path
     folder_path=$(dirname "$file_path")
     
@@ -20,13 +20,19 @@ find "$base_dir" -type f -name "*.xml" | while read -r file_path; do
     # Get the folder name (the last directory in the path)
     folder_name=$(basename "$folder_path")
 
+    # Check if the folder path contains a locale code (e.g., "en", "fr", "en-US", "fr-CA")
+    if [[ "$folder_path" =~ /[a-z]{2}(-[a-z]{2})?/ ]]; then
+        # If the folder path contains a locale code, skip this file
+        echo "Skipping file: $file_path (locale code detected)"
+        continue
+    fi
+
     # Add the dynamic push configuration to the YAML file for non-locale folders
     echo "    - file: $folder_path/$file_name" >> ./push_config.yml
     echo "      params:" >> ./push_config.yml
-    echo "        file_format: xml" >> ./push_config.yml
-    echo "        locale_id: en" >> ./push_config.yml
+    echo "        file_format: simple_json" >> ./push_config.yml
+    echo "        locale_id: 5efbe7aea90b7ea769ea0d008e3b77b0" >> ./push_config.yml
     echo "        update_translations: true" >> ./push_config.yml
     echo "        tags: $folder_name" >> ./push_config.yml
     echo "----------------------"
-
 done
