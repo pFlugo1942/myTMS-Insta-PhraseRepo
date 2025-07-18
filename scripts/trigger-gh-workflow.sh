0d14d26840662a16742243c65a60af061fa73bc0
pFlugo1942 <freddy.lugo@phrase.com>
2025-07-18 16:12:04 -0400

    Update trigger-gh-workflow.sh

--- scripts/trigger-gh-workflow.sh
+++ scripts/trigger-gh-workflow.sh
@@ -2,7 +2,7 @@
 
 # === GitHub Configuration ===
 REPO_OWNER="pFlugo1942"
-REPO_NAME="myTMS-Insta-PhraseRepo"
+REPO_NAME="myTMS-Insta-PhraseRepo.git"
 WORKFLOW_FILE_NAME="main-workflow.yml"  # Or use the workflow ID
 GITHUB_TOKEN="${GITHUB_TOKEN:?Missing GITHUB_TOKEN env var}"
 BRANCH="main"
