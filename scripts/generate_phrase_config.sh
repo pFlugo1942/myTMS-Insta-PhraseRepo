8f5375013e0c0fb90a396cba470a0ec6a1004702
pFlugo1942 <freddy.lugo@phrase.com>
2025-07-18 18:18:07 -0400

    Update generate_phrase_config.sh

--- scripts/generate_phrase_config.sh
+++ scripts/generate_phrase_config.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Specify the base directory
-base_dir="./src"
+base_dir="./instashopper-android/shared"
 
 # Initialize the configuration file
 echo "phrase:" > ./push_config.yml
-echo "  project_id: aa17ae5c0c19b1bd0d3cbe9e779d210a" >> ./push_config.yml
-echo "  file_format: simple_json" >> ./push_config.yml
+echo "  project_id: 15d32bafd4ffe92f156bcca0549a07e6" >> ./push_config.yml
+echo "  file_format: xml" >> ./push_config.yml
 echo "  push:" >> ./push_config.yml
 
 # Loop over all JSON files in nested directories
-find "$base_dir" -type f -name "*.json" | while read -r file_path; do
+find "$base_dir" -type f -name "*.xml" | while read -r file_path; do
     # Get the full folder path
     folder_path=$(dirname "$file_path")
     
@@ -30,9 +30,16 @@ find "$base_dir" -type f -name "*.json" | while read -r file_path; do
     # Add the dynamic push configuration to the YAML file for non-locale folders
     echo "    - file: $folder_path/$file_name" >> ./push_config.yml
     echo "      params:" >> ./push_config.yml
-    echo "        file_format: simple_json" >> ./push_config.yml
-    echo "        locale_id: 5efbe7aea90b7ea769ea0d008e3b77b0" >> ./push_config.yml
+    echo "        file_format: xml" >> ./push_config.yml
+    echo "        locale_id: en" >> ./push_config.yml
     echo "        update_translations: true" >> ./push_config.yml
     echo "        tags: $folder_name" >> ./push_config.yml
     echo "----------------------"
+
+    # Append to pull configuration
+    echo "    - file: $folder_path-<locale_code>/strings.xml" >> pull_config.yml
+    echo "      params:" >> pull_config.yml
+    echo "        file_format: xml" >> pull_config.yml
+    echo "        tags: $folder_name" >> pull_config.yml
+
 done
